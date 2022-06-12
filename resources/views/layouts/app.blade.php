9dfa05a1c5cce03712772a56573c607e9d3803e0
Alberto Núñez <albertonunezmaneiro@gmail.com>
2022-06-12 15:28:04 -0400

    Creando formulario de registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,12 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">
-                    @yield('titulo')
+                    DevStagram
                 </h1>
 
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuentas</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuentas</a>
                 </nav>
             </div>
         </header>
@@ -29,7 +29,7 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
             DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>
     </body>
