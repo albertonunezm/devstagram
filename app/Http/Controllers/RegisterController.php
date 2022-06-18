77e6c61b26abbc0c813d4b9d05195ae8cc2ba16c
Alberto Núñez <albertonunezmaneiro@gmail.com>
2022-06-18 12:32:06 -0400

    Terminando el Registro de Usuario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -23,6 +26,13 @@ class RegisterController extends Controller
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
             'password' => 'required|confirmed|min:6'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => Str::lower($request->username),
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
     }
 }
