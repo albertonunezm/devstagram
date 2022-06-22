49a24f8cbe81cb7b8cb4694a40a1e9bdadddc390
Alberto Núñez <albertonunezmaneiro@gmail.com>
2022-06-22 02:33:05 -0400

    Dropzone Instalado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @stack('styles')
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <title>DevStagram - @yield('titulo')</title>
         <script src="{{ asset('js/app.js') }}"></script>
