90aaf7696f3bb52c3d0f67e92125ca8102eba3e2
Alberto Núñez <albertonunezmaneiro@gmail.com>
2022-08-13 21:51:48 -0400

    Terminando funcionalidad de seguidor/siguiendo

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PerfilController;
 
@@ -49,3 +50,7 @@ Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.sto
 // Like a las fotos
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+//Siguiendo Usuarios
+Route::post('/{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
+Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');
